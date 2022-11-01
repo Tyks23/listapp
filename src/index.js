6b61329ec2f0d72a2d2de9ad3f36880916fb8fc0
Tyks23 <epic.raider99@gmail.com>
2022-11-01 21:35:47 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -10,10 +8,8 @@ const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <React.StrictMode>   
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
