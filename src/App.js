0b09193ea401f8d8e3f23fcda43f12146dc8a7c0
Tyks23 <epic.raider99@gmail.com>
2022-11-01 21:30:58 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,84 @@
 import axios from 'axios';
-import { React ,useState, useRef } from 'react'
+import { React, useState, useEffect, useCallback } from 'react'
 import './App.css';
 import ListComponent from './components/ListComponent';
 
 
-function getItemsFromWeb(){
-  axios.get('https://my.api.mockaroo.com/shipments.json?key=5e0b62d0')
-  .then(function (response) {
-    // handle success
-    return response.data;
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+async function getItemsFromWeb() {
+  return axios.get('https://my.api.mockaroo.com/shipments.json?key=5e0b62d0')
+    .then(function ({ data, status }) {
+      // handle success
+      return { status, data };
+    })
+    .catch(function ({ response: { data, status } }) {
+      // handle error
+      return { status, data }
+    })
 }
 
-async function getItemsFromText(){
-  await fetch('/Shipments.txt')
-    .then((r) => r.text())
-    .then(text  => {   
-      return JSON.parse(text);
-    })  
-}
+async function getItemsFromText() {
+  return fetch('/Shipments.txt')
+    .then((r) => r.json());
 
+}
 
 function App() {
 
   const [listItems, setListItems] = useState([]);
-  const [show, setShow] = useState(true);
-  
+
+  const removeElement = (orderNo) => {
+    const newListItems = listItems.filter((order) => order.orderNo !== orderNo);
+    setListItems(newListItems);
+  };
+
+  const changeElement = (orderNo, newData) => {
+    const newListItems = listItems.map((e) => {
+      if (e.orderNo !== orderNo) return e;
+      return newData;
+    }, [])
+    setListItems(newListItems);
+  }
+
+  const getData = useCallback(async () => {
+    const response = await getItemsFromWeb()
+    //troubleshooting limiter
+    // const t = response.status === 200 ? Object.values(response.data) : await getItemsFromText()
+    // setListItems(t.map((e, i) => i < 3 ? e : null, []).filter((e) => e !== null))
+    setListItems(response.status === 200 ? Object.values(response.data) : await getItemsFromText())
+  }, [])
+
+  useEffect(() => {
+    getData()
+  }, [getData])
+
   return (
     <div className="App">
-      <button onClick={async () => {
-        console.log(listItems);
-        setListItems(getItemsFromText)
-        console.log(listItems)}}>test</button>
+      <table>
+        <thead>
+          <tr>
+            <th>OrderNo</th>
+            <th>Date</th>
+            <th>Customer</th>
+            <th>Cosignee</th>
+            <th>Status</th>
+            <th>TrackingNo</th>
+            <th>Delete</th>
+            <th>View Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listItems.map(({ orderNo, date, customer, consignee, status, trackingNo }) => <ListComponent key={orderNo}
+            orderNo={orderNo}
+            date={date}
+            customer={customer}
+            consignee={consignee}
+            status={status}
+            trackingNo={trackingNo}
+            deleteCallback={removeElement}
+            detailCallback={changeElement} />, [])}
+        </tbody>
+      </table>
 
-<button onClick={async () => {console.log(listItems); setShow(!show)}}>test2</button>
-
-      {show ? <p>nothing here</p> : listItems.map(({ orderNo, date, customer, consignee, status, trackingNo }) => 
-      <ListComponent key={orderNo} 
-      orderNo={orderNo} 
-      date={date}
-      customer={customer}
-      consignee={consignee}
-      status={status}
-      trackingNo={trackingNo}/>, [])}
     </div>
   );
 }
--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -1,22 +1,55 @@
-import "./ListComponent.css";
+import { React, useState, useEffect } from 'react'
+
 
 export default function ListComponent(
-    orderNo, 
-    deliverydate, 
-    customer, 
-    trackingNo, 
-    status, 
-    consignee) {
+  { orderNo,
+    date,
+    customer,
+    trackingNo,
+    status,
+    consignee,
+    deleteCallback,
+    detailCallback }) {
 
+  const [detailView, setDetailView] = useState(false);
+  if (!detailView) {
+    return <tr>
+      <td>{orderNo}</td>
+      <td>{date}</td>
+      <td>{customer}</td>
+      <td>{trackingNo}</td>
+      <td>{status}</td>
+      <td>{consignee}</td>
 
+      <td className="tableDelete" onClick={() => { deleteCallback(orderNo) }}>Delete</td>
+      <td className="tableDetails" onClick={() => { setDetailView(!detailView) }}>Details</td>
+    </tr>
+  }
+  return <>
+    <form id={orderNo} onSubmit={(e) => {
+      e.preventDefault()
+      const { orderNo: newOrderNo, date, customer, trackingNo, status, consignee } = e.target;
+      const newData = {
+        orderNo: newOrderNo.value,
+        date: date.value,
+        customer: customer.value,
+        trackingNo: trackingNo.value,
+        status: status.value,
+        consignee: consignee.value
+      }
+      detailCallback(orderNo, newData)
+      setDetailView(!detailView)
+    }}></form>
+    <tr>
+      <td><input form={orderNo} name="orderNo" defaultValue={orderNo} /></td>
+      <td><input form={orderNo} name="date" defaultValue={date} /></td>
+      <td><input form={orderNo} name="customer" defaultValue={customer} /></td>
+      <td><input form={orderNo} name="trackingNo" defaultValue={trackingNo} /></td>
+      <td><input form={orderNo} name="status" defaultValue={status} /></td>
+      <td><input form={orderNo} name="consignee" defaultValue={consignee} /></td>
+      <td className="tableDelete" onClick={() => { deleteCallback(orderNo) }}>Delete</td>
+      <td><input form={orderNo} type="submit" value="Save details" /></td>
 
-  return <div className="listItem">
-        <p>{orderNo}</p>
-        <p>{deliverydate}</p>
-        <p>{customer}</p>
-        <p>{trackingNo}</p>
-        <p>{status}</p>
-        <p>{consignee}</p>
-  </div>
+    </tr>
+  </>
 }
-
